9db51ad72d73694473da5023a57d0325761fb0a6
Onurcan Kurt <onurcankurt96@gmail.com>
2023-12-11 17:29:15 +0300

    Initial Commit

---
+++ NetworkInterchangable/NetworkInterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangableApp.swift
+//  NetworkInterchangable
+//
+//  Created by onur on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
